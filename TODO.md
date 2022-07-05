babe36c9bc646dd711681b971bf2328a05f87427
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-07-05 09:53:48 +0530

    Created static dart board

--- TODO.md
+++ TODO.md
@@ -1,11 +1,11 @@
 # TODO
 
-- [ ] Create a static dart board
+- [ ] Make a movable arrow
 - [ ] Check the arrow and board collision
 - [ ] Stop if arrow collides the board
-- [ ] Make a movable arrow
 - [ ] Log the all results in the same page
 
 # DONE
 
+- [x] Create a static dart board
 - [x] Create a movable arrow
--- src/dart.js
+++ src/dart.js
@@ -1,33 +1,84 @@
-const updateArrowPosition = (arrow) => {
-  const arrowElement = document.getElementById(arrow.id);
+const onload = () => {
+  const px = (value) => value + 'px';
 
-  arrowElement.style.left = arrow.position.left + 'px';
-  arrowElement.style.top = arrow.position.top + 'px';
-};
+  const isInBoard = (arrow, board) => {
+    const { top } = arrow.position;
+    const start = board.position.top;
+    const end = board.position.top + board.height;
 
-const moveArrow = (arrow) => {
-  if (arrow.position.left < 100) {
-    arrow.speed.dx = 0;
-  }
-  arrow.position.left -= arrow.speed.dx;
-};
-
-const main = () => {
-  const arrow = {
-    id: 'arrow',
-    position: {
-      top: 250,
-      left: 400
-    },
-    speed: {
-      dx: 2
+    if (top < start || top > end) {
+      alert('lost!');
+      return;
     }
+    alert('Won');
   };
 
-  setInterval(() => {
-    moveArrow(arrow);
-    updateArrowPosition(arrow);
-  }, 33);
-}
+  const drawBoard = (board, parentId) => {
+    const boardElement = document.createElement('div');
+    boardElement.id = board.id;
+    boardElement.style.top = px(board.position.top);
+    boardElement.style.left = px(board.position.left);
+    boardElement.style.width = 0;
+    boardElement.style.boxSizing = 'border-box';
+    boardElement.style.height = board.height;
+    boardElement.style.border = '1px solid red';
+    boardElement.style.position = 'relative';
 
-window.onload = main;
+    const parent = document.getElementById(parentId);
+    parent.appendChild(boardElement);
+  };
+
+  const updateArrowPosition = (arrow) => {
+    const arrowElement = document.getElementById(arrow.id);
+    const { left, top } = arrow.position;
+
+    arrowElement.style.boxSizing = 'border-box';
+    arrowElement.style.left = px(left);
+    arrowElement.style.top = px(top);
+  };
+
+  const moveArrow = (arrow) => {
+    if (arrow.position.left <= 100) {
+      arrow.speed.dx = 0;
+      return;
+    }
+    arrow.position.left -= arrow.speed.dx;
+  };
+
+  const main = () => {
+    const arrow = {
+      id: 'arrow',
+      position: {
+        top: 301,
+        left: 400
+      },
+      speed: {
+        dx: 1
+      }
+    };
+
+    const board = {
+      id: 'board',
+      position: {
+        top: 100,
+        left: 100
+      },
+      height: 200,
+      width: 0
+    };
+
+    const intervalId = setInterval(() => {
+      moveArrow(arrow);
+      updateArrowPosition(arrow);
+
+      if (arrow.speed.dx === 0) {
+        clearInterval(intervalId);
+        isInBoard(arrow, board);
+      }
+    }, 3);
+
+    drawBoard(board, 'container');
+  }
+  main();
+}
+window.onload = onload;
